523de78eaf77cf51c139f968a3cddf0e21c64263
PGijsbers <p.gijsbers@tue.nl>
2025-11-05 15:21:08 +0100

    Update based on hugging face connector

--- README.md
+++ README.md
@@ -1,43 +1,52 @@
-# openml-connector
-Synchronizes metadata of OpenML assets with AI-on-Demand.
-This project is work in progress.
+# AI-on-Demand Hugging Face connector
+Collects dataset metadata from [Hugging Face](https://huggingface.co) and uploads it to AI-on-Demand.
 
+This package is not intended to be used directly by others, but may serve as an example of how to build a connector for the AI-on-Demand platform.
+For more information on how to test this connector locally as a springboard for developing your own connector, reference the [Development](#Development) section below.
 
-## Usage
-Simply invoke the script and specify which asset types to synchronize (default: all supported types) and whether you want to only register new assets or update modified existing assets (default: only add new assets). For example:
+### TODO
+This package is work in progress.
 
-`python sync.py --type dataset --type model --mode add`
-
-Note that updating existing assets, e.g., with `--mode all`, requires significantly more network requests and so will take much longer and put additional burden on the OpenML server.
-
-It is going to support the following synchronization modes:
-
- - add: register only new assets, do not modify existing ones.
- - update: update existing assets, e.g., if their description changed.
- - remove: remove assets which no longer exist on the platform.
- - all: perform all currently supported synchronization modes for each asset type.
-
-And it will be able to synchronize the following asset types:
-
- - dataset: from the OpenML dataset
- - model: from the OpenML flow
- - experiment: from the OpenML run
-
-The package is work in progress, reference the table below (x is done, - is to do):
-
-| Sync Mode | Dataset | Model | Experiment |
-|-----------|---------|-------|------------|
-| Add       |    -    |   -   |     -      |
-| Update    |    -    |   -   |     -      |
-| Remove    |    -    |   -   |     -      |
-
-
-note: Always refer to `python main.py --help` for the most recent instructions.
+- [ ] Automatically publish to DockerHub on release
+- [ ] Add tests
 
 ## Installation
-Invoke the following command in a dedicated virtual environment: `python -m pip install -r requirements.txt`
+You can use the image directly from Docker Hub (TODO) or build it locally.
 
-## Configuration
-Configuration happens through environment variables, which may be defined in environment files.
-We recommend using a file named `.env.override` for your non-default configurations, and use `.env` only as a reference to see which variables should be set.
+From Docker Hub: `docker pull aiondemand/openml-connector`.
 
+To build a local image:
+
+ - Clone the repository: `git clone https://github.com/aiondemand/openml-connector && cd openml-connector`
+ - Build the image: `docker build -t aiondemand/openml-connector -f Dockerfile .`
+
+### Configuring Client Credentials
+You will need to configure what server the connector should connect to, as well as the credentials for the client that allow you to upload data.
+The connector requires a `config.toml` file with a valid [aiondemand configuration](https://aiondemand.github.io/aiondemand/api/configuration/),
+the default configuration can be found in the [`/script/config.prod.toml`](/script/config.prod.toml) file.
+You will also need to have the 'Client Secret' for the client, which can be obtained from the keycloak administrator.
+The client secret must be provided to the Docker container as an environment variable or in a dotenv file *similar to* [`script/.local.env`](script/.local.env) but named `script/.prod.env`.
+
+Please contact the Keycloak service maintainer to obtain said credentials you need if you are in charge of deploying this Hugging Face connector.
+
+## Running the Connector
+You will need to mount the aiondemand configuration to `/home/appuser/.aiod/config.toml` and provide environment variables directly with `-e` or through mounting the dotfile in `/home/appuser/.aiod/openml/.env`. The [`script/run.sh`](script/run.sh) script provides a convenience that automatically does this. 
+It takes one positional argument that has to be `local`, `test`, or `prod` to use the respective files in the `script` folder for configuration.
+Any following arguments are interpreted as arguments to the main script.
+For the latest commandline arguments, use `docker run aiondemand/openml-connector --help`.
+Some example invocations that use the `script/run.sh` script:
+
+ - `script/run.sh local --mode id --value 61 --app-log-level debug` syncs one specific openml dataset, and produces debug logs for the connector only.
+ - `script/run.sh test --mode since --value 100 --root-log-level debug` syncs all datasets with identifier 100 or greater (in ascending order).
+ - `script/run.sh prod --mode all --root-log-level info` indexes all datasets on OpenML, producing info logs for the connector and all its dependencies (this is the default).
+
+## Development
+You can test the connector when running the [metadata catalogue](https://github.com/aiondemand/aiod-rest-api) locally.
+The default configurations for this setup can be found in the [`.local.env`](script/.local.env) and [`config.local.toml`](script/config.local.toml) files.
+
+When connecting to the AI-on-Demand test or production server, you will need to a dedicated client registered in the keycloak instance which is connected to the REST API you want to upload data to. 
+See [this form]() to apply for a client. The client will need to have a `platform_X` role attached, where `X` is the name of the platform from which you register assets. 
+When a client is created, you will need its 'Client ID' and 'Client Secret' and update the relevant configuration and environment files accordingly.
+
+## Disclaimer
+This project is not affiliated with OpenML in any way.
---
+++ src/connector.py
@@ -0,0 +1,313 @@
+import datetime as dt
+import time
+from datetime import datetime
+import os
+
+import dateutil.parser
+from argparse import ArgumentParser
+from http import HTTPStatus
+from pathlib import Path
+import logging
+from enum import StrEnum, auto
+
+import aiod
+from aiod.authentication import set_token, Token
+from dotenv import load_dotenv
+import requests
+
+
+class Modes(StrEnum):
+    ALL = auto()
+    SINCE = auto()
+    ID = auto()
+
+
+logger = logging.getLogger(__name__)
+REQUEST_TIMEOUT = 10
+MAX_TEXT = 65535
+MAX_DESCRIPTION_LENGTH = 1800
+PLATFORM_NAME = "openml"
+STOP_ON_UNEXPECTED_ERROR = False
+PER_DATASET_DELAY = None
+
+
+class ParsingError(Exception):
+    pass
+
+
+class ServerError(Exception):
+    pass
+
+
+def list_datasets(from_: int | None = None):
+
+    def paginate_all_datasets(items_per_page: int = 50):
+        url_data = f"https://www.openml.org/api/v1/json/data/list/limit/{items_per_page}/offset/{{offset}}"
+        for offset in range(0, 1_000_000, items_per_page):
+            response = requests.get(url_data.format(offset=offset), timeout=REQUEST_TIMEOUT)
+            if not response.ok:
+                status_code = response.status_code
+                msg = response.json()["error"]["message"]
+                err_msg = f"Error while fetching {url_data} from OpenML: ({status_code}) {msg}"
+                raise ServerError(err_msg)
+            logger.debug(f"Paging through datasets (offset {offset})")
+
+            try:
+                dataset_summaries = response.json()["data"]["dataset"]
+                if dataset_summaries:
+                    yield from dataset_summaries
+                    logger.debug(f"Paged through datasets (total {len(dataset_summaries)})")
+                else:
+                    break
+            except Exception:
+                raise ParsingError(f"Could not parse response ({response.status_code}): {response.content}", exc_info=True)
+
+    from_ = from_ or 0
+    for dataset in paginate_all_datasets():
+        try:
+            identifier = dataset["did"]
+            if identifier < from_:
+                continue
+        except KeyError:
+            logger.error(f"Received invalid summary: {dataset}", exc_info=True)
+            continue
+
+        try:
+            yield fetch_openml_dataset(identifier, dataset["quality"])
+        except Exception as e:
+            logger.error(f"Exception when processing dataset {identifier}")
+            logger.exception(e)
+
+
+def fetch_openml_dataset(identifier_: int, qualities: dict | None = None):
+    if not qualities:
+        qualities_url = f"https://www.openml.org/api/v1/json/data/qualities/{identifier_}"
+        qualities_response = requests.get(
+            qualities_url,
+            timeout=REQUEST_TIMEOUT,
+        )
+        if not qualities_response.ok:
+            status_code = qualities_response.status_code
+            try:
+                msg = qualities_response.json()["error"]["message"]
+            except:
+                msg = response.content
+            err_msg = f"Error while fetching {qualities_url} from OpenML: ({status_code}) {msg}"
+            raise ServerError(err_msg)
+        try:
+            qualities = qualities_response.json()["data_qualities"]["quality"]
+        except:
+            raise ParsingError(f"Error parsing JSON of qualities of dataset {qualities_response.content}")
+
+    url_data = f"https://www.openml.org/api/v1/json/data/{identifier_}"
+    response = requests.get(url_data, timeout=REQUEST_TIMEOUT)
+
+    if not response.ok:
+        status_code = response.status_code
+        msg = response.json()["error"]["message"]
+        err_msg = f"Error while fetching {url_data} from OpenML: ({status_code}) {msg}"
+        raise ServerError(err_msg)
+
+    try:
+        dataset_json = response.json()["data_set_description"]
+        qualities_json = {quality["name"]: quality["value"] for quality in qualities}
+        return dataset_json | {"qualities": qualities_json}
+    except Exception:
+        raise ParsingError(f"Error parsing JSON of dataset {response.content}")
+
+
+def _convert_dataset_to_aiod(dataset: dict) -> dict:
+    identifier = dataset["id"]
+
+    description = dataset["description"]
+    if isinstance(description, list) and len(description) == 0:
+        description = ""
+    if not isinstance(description, str):
+        logger.warning(f"Ignoring description {description} of dataset {identifier}.")
+        description = ""
+    if len(description) > MAX_DESCRIPTION_LENGTH:
+        text_break = " [...]"
+        description = description[: MAX_DESCRIPTION_LENGTH - len(text_break)] + text_break
+
+    size = None
+    if n_rows := dataset["qualities"].get("NumberOfInstances"):
+        size = {
+            "unit": "instances",
+            "value": int(float(n_rows)),  # OpenML adds the decimal: xxx.0
+        }
+
+    return dict(
+        platform=PLATFORM_NAME,
+        platform_resource_identifier=identifier,
+        name=dataset["name"],
+        version=dataset["version"],
+        same_as=f"https://www.openml.org/api/v1/json/data/{identifier}",
+        description=dict(plain=description),
+        date_published=dateutil.parser.parse(dataset["upload_date"]).isoformat(),
+        license=dataset.get("licence"),
+        distribution=[dict(content_url=dataset["url"], encoding_format=dataset["format"])],
+        is_accessible_for_free=True,
+        keyword=dataset.get("tag", []),
+        size=size,
+    )
+
+
+def upsert_dataset(dataset: dict) -> int:
+    identifier = dataset["id"]
+    try:
+        local_dataset = _convert_dataset_to_aiod(dataset)
+
+        try:
+            aiod_dataset = aiod.datasets.get_asset_from_platform(
+                platform=PLATFORM_NAME,
+                platform_identifier=identifier,
+                data_format="json",
+            )
+        except KeyError:
+            response = aiod.datasets.register(metadata=local_dataset)
+            if isinstance(response, str):
+                logger.debug(f"Indexed dataset {identifier}: {response}")
+                return HTTPStatus.OK
+            elif isinstance(response, requests.Response):
+                logger.warning(f"Error uploading dataset ({response.status_code}: {response.content}")
+                breakpoint()
+                return response.status_code
+            raise
+
+        if "identifier" not in aiod_dataset:
+            raise RuntimeError(
+                f"Unexpected server response retrieving OpenML dataset {identifier} "
+                f"from AI-on-Demand: {aiod_dataset}"
+            )
+
+        response = aiod.datasets.replace(identifier=aiod_dataset['identifier'], metadata=local_dataset)
+        if response.status_code == HTTPStatus.OK:
+            logger.debug(f"Updated dataset {identifier}: {aiod_dataset['identifier']}")
+        else:
+            logger.warning(f"Could not update {aiod_dataset['identifier']} for openml dataset {identifier} "
+                           f"({response.status_code}): {response.content}")
+    except Exception as e:
+        logger.exception(
+            msg=f"Exception encountered when upserting dataset {identifier}.",
+            exc_info=e,
+        )
+        if STOP_ON_UNEXPECTED_ERROR:
+            raise
+        return -1
+    return response.status_code
+
+
+def parse_args():
+    parser = ArgumentParser()
+
+    parser.add_argument(
+        "--mode",
+        choices=list(Modes),
+    )
+    parser.add_argument(
+        "--value",
+        default=None,
+        required=False,
+        type=str,
+        help=(
+            "For mode 'ID' this must be an openml identifier. "
+            "For mode 'SINCE' this must be an openml identifier, this dataset and "
+            "all datasets with higher identifier will be indexed."
+            "Cannot be set with mode 'ALL'."
+        )
+    )
+    log_levels = [level.lower() for level in logging.getLevelNamesMapping()]
+    parser.add_argument(
+        "--app-log-level",
+        choices=log_levels,
+        default='info',
+        help="Emit all log messages generated of at least this level by the app."
+    )
+    parser.add_argument(
+        '--root-log-level',
+        choices=log_levels,
+        default='error',
+        help="Emit all log messages generated of at least this level by the app's dependencies."
+    )
+    args = parser.parse_args()
+    if args.mode == Modes.ALL and args.value:
+        logger.error("Cannot run mode 'all' when a value is supplied.")
+        quit(code=1)
+
+    return args
+
+
+def configure_connector():
+    global BATCH_SIZE, PLATFORM_NAME, STOP_ON_UNEXPECTED_ERROR, PER_DATASET_DELAY
+
+    dot_file = Path("~/.aiod/openml/.env").expanduser()
+    if dot_file.exists() and load_dotenv(dot_file):
+        logger.info(f"Loaded variables from {dot_file}")
+    else:
+        reason = "unknown reason" if dot_file else "file does not exist"
+        logger.info(f"No environment variables loaded from {dot_file}: {reason}.")
+
+    BATCH_SIZE = os.getenv("AIOD_BATCH_SIZE", 25)
+    PLATFORM_NAME = os.getenv("PLATFORM_NAME", PLATFORM_NAME)
+    PER_DATASET_DELAY = float(delay) if (delay := os.getenv("PER_DATASET_DELAY")) else None
+    STOP_ON_UNEXPECTED_ERROR = os.getenv("STOP_ON_UNEXPECTED_ERROR", STOP_ON_UNEXPECTED_ERROR)
+
+    token = os.getenv("CLIENT_SECRET")
+    assert token, "CLIENT_SECRET environment variable not set"
+
+    masked_token = '*' * (len(token) + 4) + token[-4:]
+    logger.info(f"{'aiondemand version:':25} {aiod.version}")
+    logger.info(f"{'STOP_ON_UNEXPECTED_ERROR:':25} {STOP_ON_UNEXPECTED_ERROR}")
+    logger.info(f"{'PER_DATASET_DELAY:':25} {PER_DATASET_DELAY}")
+    logger.info(f"{'AI-on-Demand API server:':25} {aiod.config.api_server}")
+    logger.info(f"{'Platform Name:':25} {PLATFORM_NAME}")
+    logger.info(f"{'Authentication server:':25} {aiod.config.auth_server}")
+    logger.info(f"{'Client ID:':25} {aiod.config.client_id}")
+    logger.info(f"{'Using secret:':25} {masked_token}")
+
+    set_token(Token(client_secret=token))
+    user = aiod.get_current_user()
+
+    required_role = f"platform_{PLATFORM_NAME}"
+    wrong_platform_msg = (
+        f"Client roles {user.roles} do not include required {required_role!r} role."
+        "Please make sure the `PLATFORM_NAME` environment variable is configured correctly, "
+        "or contact your Keycloak administrator."
+    )
+    assert required_role in user.roles, wrong_platform_msg
+
+    logger.info("Successfully authenticated and connected to AI-on-Demand.")
+
+
+def main():
+    args = parse_args()
+    logging.basicConfig(level=args.root_log_level.upper())
+    logger.setLevel(args.app_log_level.upper())
+    configure_connector()
+
+    match (args.mode, args.value):
+        case Modes.ID, id_:
+            if not id_.isdigit():
+                logger.error(f"Identifier specified should be an integer, is {id_!r}")
+                quit(1)
+            dataset = fetch_openml_dataset(int(id_))
+            upsert_dataset(dataset)
+        case Modes.SINCE, id_:
+            if not id_.isdigit():
+                logger.error(f"Identifier specified should be an integer, is {id_!r}")
+                quit(1)
+            for dataset in list_datasets(from_=int(id_)):
+                upsert_dataset(dataset)
+                if PER_DATASET_DELAY:
+                    time.sleep(PER_DATASET_DELAY)
+        case Modes.ALL, None:
+            for dataset in list_datasets():
+                upsert_dataset(dataset)
+                if PER_DATASET_DELAY:
+                    time.sleep(PER_DATASET_DELAY)
+        case _:
+            raise NotImplemented(f"Unexpected arguments: {args}")
+
+
+if __name__ == '__main__':
+    main()
